fa88e338edf2e450b40c94d31e731d540848bbbb
jpsr-6x1 <joaopedrosilva12987@gmail.com>
2025-11-19 21:54:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
-const livro = {
-    titulo : "A Cabana",
-    autor : "William P. Young",
-    paginas : "240 páginas",
-    disponivel: "Sim",
-    
-    detalhes() {
-        return `O livro "${this.titulo}" foi escrito por ${this.autor}, possui ${this.paginas} e é publicado pela ${this.editora}.`;
-    }
-};
+let listaDeCompras = ["arroz", "feijao", "macarrao", "leite", "banana"];
 
-console.log(livro.titulo);
-console.log(livro["autor"]);
+console.log("Lista inicial:", listaDeCompras);
 
-livro.editora = "Editora Arqueiro";
+listaDeCompras.push("chocolate");
+console.log("Após adicionar item:", listaDeCompras);
 
-console.log(livro.detalhes());
+listaDeCompras.shift();
+console.log("Após remover o primeiro item:", listaDeCompras);
 
-const biblioteca = {
-    livros: ["A Cabana","Diario de um Banana","Dom Quixote","O Menino do Dedo Verde"],
-}
+let itensComMaisDeCincoCaracteres = listaDeCompras.filter(item => item.length > 5);
 
-for (let livro of biblioteca.livros) {
-    console.log(livro);
-}
-// Aula 16 //
-function saudar(nome = "Pedro") {
-    return `Olá, ${nome}!`;
-}
+console.log("Itens com mais de 5 caracteres:", itensComMaisDeCincoCaracteres);
 
-function calcularMedia (a,b,c){
-    return(a+b+c)/3;
-}
+//Atividade 2
 
-function apresentarProduto (nomeProduto,preco = 0){
-    return `Produto: ${nomeProduto} | Preço: R$ ${preco}`;
-}
+let frase = " JavaScript é uma linguagem poderosa! ";
 
-console.log(saudar("Ana"));  
-console.log(saudar("Carlos"));
+let fraseSemEspacos = frase.trim();
 
-console.log(calcularMedia(8, 7.5, 9));  
-console.log(calcularMedia(10, 6, 8));
+let palavras = fraseSemEspacos.split(" ");
 
-console.log(apresentarProduto("Caderno", 12.50));  
-console.log(apresentarProduto("Caneta"));
+console.log("Frase sem espaços:", fraseSemEspacos);
+console.log("Array de palavras:", palavras);
